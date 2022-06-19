84dd01705e3a41d26b474cda93eb5318af24ab09
dzuizz <59071340+dzuizz@users.noreply.github.com>
2022-06-19 17:06:08 +0800

    Solved Kefa and First Steps

--- A_Kefa_and_First_Steps.cpp
+++ A_Kefa_and_First_Steps.cpp
@@ -1,6 +1,26 @@
-#include <iostream>
+#include<iostream>
+#include<vector>
 using namespace std;
-typedef long long ll;
+
+int ind = 1, pre, cur = 1, ans, n;
 
 int main() {
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    pre = a[0];
+    
+    while (ind < n) {
+        if (a[ind] < pre) {
+            ans = max(ans, cur);
+            pre = a[ind];
+            cur = 1;
+            ind++;
+            continue;
+        }
+        pre = a[ind];
+        cur++, ind++;
+    }
+    cout << max(cur, ans);
 }
