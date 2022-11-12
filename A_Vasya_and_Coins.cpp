a8259ec5a7584b9396e8c42a62e37fb02a70c1b9
dzuizz <59071340+dzuizz@users.noreply.github.com>
2022-11-12 16:52:17 +0800

    Solved Vasya and Coins

---
+++ A_Vasya_and_Coins.cpp
@@ -0,0 +1,67 @@
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <climits>
+#include <cstdio>
+#include <cmath>
+#include <unordered_map>
+#include <vector>
+#include <stack>
+#include <deque>
+#include <queue>
+#include <map>
+#include <set>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef map<ll, ll> mll;
+typedef set<ll> sll;
+typedef queue<ll> qll;
+typedef queue<pll> qpll;
+
+#define all(x) x.begin(), x.end()
+#define sz(x) (ll) x.size()
+#define fr front()
+#define bk back()
+#define pb push_back
+#define pf push_front
+#define fi first
+#define se second
+#define FOR(i, a, b, inc) for (ll i = a; i < b; i += inc)
+#define RFOR(i, a, b, inc) for (ll i = b - 1; i >= a; i -= inc)
+#define REP(i, a) FOR(i, 0, a, 1)
+#define RREP(i, a) RFOR(i, 0, a, 1)
+#define each(a, x) for (auto& a: x)
+
+const ll INF = 1e9;
+const ll M = 1e9 + 7;
+
+ll gcd(ll a, ll b) {
+    if (b == 0) return a;
+    return gcd(b, a%b);
+}
+ll pow(ll a, ll b) {
+    if (b == 0) return 1;
+    if (b == 1) return a;
+    ll x = pow(a, b/2);
+    if (b%2 == 0) return x*x;
+    return x*x*a;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    ll t; cin >> t;
+    while (t--) {
+        ll a, b; cin >> a >> b;
+
+        if (!a) cout << 1 << '\n';
+        else cout << a + 2 * b + 1 << '\n';
+    }
+}
