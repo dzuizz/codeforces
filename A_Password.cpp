c09a007178f6222227b89aab18b0c8e403ac978c
dzuizz <59071340+dzuizz@users.noreply.github.com>
2022-12-03 12:59:54 +0700

    Solved Password

---
+++ A_Password.cpp
@@ -0,0 +1,62 @@
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <climits>
+#include <cstdio>
+#include <cmath>
+#include <unordered_map>
+#include <vector>
+#include <stack>
+#include <deque>
+#include <queue>
+#include <map>
+#include <set>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef map<ll, ll> mll;
+typedef set<ll> sll;
+typedef queue<ll> qll;
+typedef queue<pll> qpll;
+
+#define all(x) x.begin(), x.end()
+#define sz(x) (ll) x.size()
+#define fr front()
+#define bk back()
+#define pb push_back
+#define pf push_front
+#define fi first
+#define se second
+#define FOR(i, a, b, inc) for (ll i = a; i < b; i += inc)
+#define RFOR(i, a, b, inc) for (ll i = b - 1; i >= a; i -= inc)
+#define REP(i, a) FOR(i, 0, a, 1)
+#define RREP(i, a) RFOR(i, 0, a, 1)
+#define each(a, x) for (auto& a: x)
+
+const ll INF = 1e9;
+const ll M = 1e9 + 7;
+
+ll f(ll n) {
+    if (n == 0) return 1;
+    return n*f(n - 1);
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    ll t; cin >> t;
+    while (t--) {
+        ll n; cin >> n;
+        ll a[n];
+
+        REP(i, n) cin >> a[i];
+
+        cout << 6 * f(10 - n) / f(2) / f(8 - n) << '\n';
+    }
+}
